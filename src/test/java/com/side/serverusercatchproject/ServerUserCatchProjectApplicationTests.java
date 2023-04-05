a9df041c5685cf05177846b25dc2692e2cef5e43
khh5762 <khh5762@naver.com>
2023-04-05 19:19:51 +0900

    Initialized Commit

---
+++ src/test/java/com/side/serverusercatchproject/ServerUserCatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.serverusercatchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerUserCatchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
